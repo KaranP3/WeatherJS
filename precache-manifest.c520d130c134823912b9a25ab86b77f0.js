09758d9714a850776fc8b477f4c74b8e94cbe860
KaranP3 <karanpillai93@gmail.com>
2019-03-14 16:34:27 +0530

    Updates

---
+++ precache-manifest.c520d130c134823912b9a25ab86b77f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/WeatherJS/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "634d97191ed82931fabe5113951770fb",
+    "url": "/WeatherJS/static/media/design-art-2029562_1280.634d9719.png"
+  },
+  {
+    "revision": "4f46da752067a217b7e6",
+    "url": "/WeatherJS/static/js/runtime~main.4f46da75.js"
+  },
+  {
+    "revision": "9279104aa1584be6a754",
+    "url": "/WeatherJS/static/js/main.9279104a.chunk.js"
+  },
+  {
+    "revision": "e2a4fca7c1285f971d4a",
+    "url": "/WeatherJS/static/js/2.e2a4fca7.chunk.js"
+  },
+  {
+    "revision": "9279104aa1584be6a754",
+    "url": "/WeatherJS/static/css/main.01656225.chunk.css"
+  },
+  {
+    "revision": "b075d6ecec262a1b5427e851f55fa157",
+    "url": "/WeatherJS/index.html"
+  }
+];
